f36a0434e11406a30f6be92f467705f6b74fe304
yueveron <yueveron@gmail.com>
2019-04-02 09:40:01 +0800

    Update sidebar.vue some change

--- src/views/SideBar.vue
+++ src/views/SideBar.vue
@@ -1,19 +1,12 @@
 <template>
-	<div>
-     <!-- normal: v-bind:list='list'
+  <div>
+    <!-- normal: v-bind:list='list'
           with vuex : v-model='myList' 
       -->
-		<draggable
-      v-model='myList' 
-      ghost-class="ghost"
-      @end="onEnd"
-    >
-			<SideItem v-for="todo in myList" 
-                :todo="todo" 
-                :key="todo.id">
-      </SideItem>
-		</draggable>
-	</div>
+    <draggable v-model="myList" ghost-class="ghost" @end="onEnd">
+      <SideItem v-for="todo in myList" :todo="todo" :key="todo.id"> </SideItem>
+    </draggable>
+  </div>
   <!-- <div>
     <SideItem v-for="todo in list" 
               :todo="todo" 
@@ -34,33 +27,32 @@ export default {
   },
   computed: {
     myList: {
-      get() {
+      get () {
         return this.list
       },
-      set(value){
+      set (value) {
         //不能通过 set value 改变 this.store.state.list，因为更新后原有的 this.store.state.list 的 vue' data 属性会被修改
         //只能在 this.store mutation.sortableList 方法内手动更新排序.
         // console.debug(value)
       }
     }
   },
-  data(){
+  data () {
     return {
       list: this.store.state.list
     }
   },
   methods: {
-    onEnd: function(evt){
+    onEnd: function (evt) {
       // console.debug ('newIndex:' + evt.newIndex, 'oldIndex:' + evt.oldIndex);
-      if(evt.newIndex != evt.oldIndex){
+      if (evt.newIndex != evt.oldIndex) {
         this.store.mutation.sortableList(evt.newIndex, evt.oldIndex);
       }
-      
-    }    
+
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  
 </style>
